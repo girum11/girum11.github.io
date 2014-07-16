325174b140ac285299bbff83383d5d61044b5f2e
Girum Ibssa <ibssagirum@gmail.com>
2014-07-15 21:45:21 -0700

    Added Cal Poly Capstone (round two, as a mentor this time). Switched some of the counts to use different scales (e.g., days since graduation -> months since graduation).

--- js/index.js
+++ js/index.js
@@ -1,14 +1,14 @@
 $(document).ready(function () {
-    var $daysSinceGraduation = $('#daysSinceGraduation');
-    var $weeksSinceStartingChordsing = $('#weeksSinceStartingChordsing');
+    var $monthsSinceGraduation = $('#monthsSinceGraduation');
+    var $monthsSinceStartingChordsing = $('#monthsSinceStartingChordsing');
     var $yearsOfAge = $('#yearsOfAge');
     var now = moment();
 
     var calculateDiff = function (startDate, unitOfTime) {
         return now.diff(startDate, unitOfTime);
-    }
+    };
 
-    $daysSinceGraduation.html(calculateDiff(moment([2013, 5, 15]), 'days'));
-    $weeksSinceStartingChordsing.html(calculateDiff(moment([2013, 10, 11]), 'weeks'));
+    $monthsSinceGraduation.html(calculateDiff(moment([2013, 5, 15]), 'months'));
+    $monthsSinceStartingChordsing.html(calculateDiff(moment([2013, 10, 11]), 'months'));
     $yearsOfAge.html(calculateDiff(moment([1990, 7, 25]), 'years'));
 });
